b8deeebffa5a2d4d5db59db846873a4f383b1f92
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2019-09-12 10:28:58 -0600

    Added Context Class

--- src/BloggingDemo-CodeFirst/BloggingDemo/Program.cs
+++ src/BloggingDemo-CodeFirst/BloggingDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BloggingDemo.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +36,20 @@ namespace BloggingDemo
             public int BlogID { get; set; }
             // Navigation Property
             public virtual Blog Blog { get; set; }
+        } // end of Post Class
+    } // end of Entities Namespace
+
+    namespace DAL
+    {
+        public class BloggingContext : DbContext
+        {
+            public BloggingContext() : base("name=BlogDb")
+            {
+
+            }
+
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
         }
     }
 }
