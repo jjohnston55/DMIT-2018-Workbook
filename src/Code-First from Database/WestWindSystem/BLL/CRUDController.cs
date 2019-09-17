d6515c94ac58882d031662f0918e7b6c50ec030f
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2019-09-17 15:08:20 -0600

    Downloaded missed class files

---
+++ src/Code-First from Database/WestWindSystem/BLL/CRUDController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    [DataObject] // for the <ObjectDataSource> control
+    public class CRUDController
+    {
+        #region Products CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Product> ListProducts()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        #endregion
+
+        #region Suppliers CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Supplier> ListSuppliers()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Suppliers.ToList();
+            }
+        }
+        #endregion
+
+        #region Categories CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Category> ListCategorys()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        #endregion
+
+        #region Addresses CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Address> ListAddresss()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Addresses.ToList();
+            }
+        }
+        #endregion
+
+    }
+}
