bc2e66779dd0bb06606e6a99e6bd6383cdf45da4
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2019-10-22 16:18:17 -0600

    Added Remedial OOP

---
+++ src/Remedial OOP/RemedialOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemedialOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var things = new Bunch<string>();
+            things.Add("Dan");
+            things.Add("Jeff");
+            things.Add("Don");
+            Console.WriteLine($"There are {things.Count} names in my list");
+            Console.ReadKey();
+        }
+    }
+}
